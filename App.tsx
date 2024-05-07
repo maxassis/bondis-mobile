5d84106937540446f943a9dd965fbe9430d8b620
max <max.assis@chatguru.com.br>
2024-05-07 18:22:02 -0300

    feat: instalando o nativewind

--- App.tsx
+++ App.tsx
@@ -1,79 +1,17 @@
-import { useEffect, useState, useRef } from 'react'
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import Maps from './src/pages/Map';
+import Profile from './src/pages/Profile';
 import { StatusBar } from 'expo-status-bar';
-import MapView, { Marker } from 'react-native-maps';
-import { StyleSheet, Text, View } from 'react-native';
-import { requestForegroundPermissionsAsync,
-  getCurrentPositionAsync, 
-  LocationObject,
-  watchPositionAsync,
-  LocationAccuracy
- } from 'expo-location';
+
 
 export default function App() {
-  const [location, setLocation] = useState<LocationObject | null>(null)
-
-  const mapRef = useRef<MapView>(null)
-
-  async function requestLocationPermissions() {
-  
-    const { granted } = await requestForegroundPermissionsAsync()
-
-    if(granted) {
-     const currentPosition = await getCurrentPositionAsync()
-     console.log(currentPosition);
-     
-     setLocation(currentPosition)
-    }
-  }
-
-  useEffect(() => {
-    requestLocationPermissions()
-  }, [])
-
-  useEffect(() => {
-    watchPositionAsync({
-      accuracy: LocationAccuracy.Highest,
-      timeInterval: 1000,
-      distanceInterval: 0
-    }, (response) => {
-      setLocation(response)
-      mapRef.current?.animateCamera({
-        center: response.coords
-      })     
-    })
-  }, [])
 
   return (
-    <View style={styles.container}>
-      {
-        location &&  
-        <MapView style={styles.map}
-        ref={mapRef}
-        initialRegion={{
-          latitude: location.coords.latitude,
-          longitude: location.coords.longitude,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421
-        }}
-        
-        >
-          <Marker coordinate={location.coords} />
-        </MapView>  
-      }
-     
-    </View>
+    <SafeAreaView className='flex-1'>
+      <Maps />
+      {/* <Profile /> */}
+      <StatusBar style="light" translucent={false}  />
+    </SafeAreaView>
+ 
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  map: {
-    flex: 1,
-    width: '100%',
-  }
-});
