4b1c778a9f6c52b526315d4427e0361ef548678c
max <max.assis@chatguru.com.br>
2024-05-18 22:07:54 -0300

    feat: fazendo o input da tela de login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,6 @@
 import { useNavigation } from "@react-navigation/native";
 import { Text, View, TextInput, SafeAreaView, TouchableOpacity } from "react-native";
+import { useForm, Controller } from "react-hook-form";
 import Close from "../../assets/Close.svg"
 import Logo from "../../assets/logo2.svg"
 import Google from "../../assets/google.svg"
@@ -8,6 +9,15 @@ import Apple from "../../assets/apple.svg"
 
 export default function Login() {
     const navigation = useNavigation<any>();
+
+    const {
+        handleSubmit,
+        control,
+        formState: { errors },
+      } = useForm();
+
+      const onSubmit = (data: any) => console.log(data);
+      
 
     return(
         <SafeAreaView className="pt-[38px] px-5 bg-white flex-1">
@@ -25,14 +35,26 @@ export default function Login() {
             <Text className="text-[#565656] mt-4 text-base">Informe seu e-mail e senha de acesso:</Text>
 
             <Text className="font-inter-bold text-base mt-8">E-mail</Text>
-            <TextInput placeholder="E-mail" className="bg-bondis-text-gray rounded-[4px] h-[52px] mt-2 pl-4"/>
+            <Controller 
+            control={control}
+            name="email"
+            render={({ field: { value, onChange } }) => (
+                <TextInput placeholder="E-mail" value={value} onChangeText={onChange} className="bg-bondis-text-gray rounded-[4px] h-[52px] mt-2 pl-4"/>  
+            )}
+            />
 
-            <Text className="mt-8 font-inter-bold text-base">Senha</Text>
-            <TextInput placeholder="Senha" className="bg-bondis-text-gray rounded-[4px] h-[52px] mt-2 pl-4"/>
+            <Text className="mt-8 font-inter-bold text-base">Senha</Text>            
+            <Controller 
+            control={control}
+            name="password"
+            render={({ field: { value, onChange } }) => (
+                <TextInput placeholder="Senha" onChangeText={onChange} value={value} className="bg-bondis-text-gray rounded-[4px] h-[52px] mt-2 pl-4"/>  
+            )}
+            /> 
 
             <Text className="mt-8 font-inter-regular text-center">Esqueceu a senha ? <Text className="font-inter-bold underline" onPress={() => navigation.navigate('Recovery')}>Recuperar</Text></Text> 
 
-            <TouchableOpacity className="h-[52px] bg-bondis-green mt-8 rounded-full justify-center items-center">
+            <TouchableOpacity onPress={handleSubmit(onSubmit)} className="h-[52px] bg-bondis-green mt-8 rounded-full justify-center items-center">
                 <Text className="font-inter-bold text-base">Entrar</Text>
             </TouchableOpacity>   
 
